0ac37922597b4fbde53551e05238ed6a443d240c
JamesMorais <capic.2022118tads0048@aluno.ifpi.edu.br>
2023-12-04 11:39:15 -0300

    ✨ Adiciona funcionalidade de notificações

--- js/script.js
+++ js/script.js
@@ -2,83 +2,41 @@ let toggleBtn = document.getElementById('toggle-btn');
 let body = document.body;
 let darkMode = localStorage.getItem('dark-mode');
 
-const enableDarkMode = () =>{
+const enableDarkMode = () => {
     toggleBtn.classList.replace('fa-sun', 'fa-moon');
     body.classList.add('dark');
     localStorage.setItem('dark-mode', 'enabled');
-}
+};
 
-const disableDarkMode = () =>{
+const disableDarkMode = () => {
     toggleBtn.classList.replace('fa-moon', 'fa-sun');
     body.classList.remove('dark');
     localStorage.setItem('dark-mode', 'disabled');
-}
+};
 
-if(darkMode === 'enabled'){
-    enableDarkMode();
-}
-toggleBtn.onclick = (e) =>{
-    darkMode =localStorage.getItem('dark-mode');
-    if(darkMode === 'disabled'){
+if (toggleBtn) {
+    toggleBtn.onclick = (e) => {
+        darkMode = localStorage.getItem('dark-mode');
+        if (darkMode === 'disabled') {
+            enableDarkMode();
+        } else {
+            disableDarkMode();
+        }
+    };
+
+    if (darkMode === 'enabled') {
         enableDarkMode();
-    }else{
-        disableDarkMode();
     }
 }
-/*
-document.addEventListener('DOMContentLoaded', function() {
+
+document.addEventListener('DOMContentLoaded', function () {
     let sideBar = document.querySelector('.side-bar');
     let body = document.body;
 
-    function updateSidebarVisibility() {
-        if (window.innerWidth >= 1200) {
-            sideBar.classList.add('active');
-            body.classList.add('active');
-        } else {
-            sideBar.classList.remove('active');
-            body.classList.remove('active');
-        }
+    if (!sideBar || !body) {
+        console.error('Elemento não encontrado. Verifique se os seletores estão corretos.');
+        return;
     }
-
-    updateSidebarVisibility();
-
-    window.addEventListener('resize', updateSidebarVisibility);
-
-    let profile = document.querySelector('.header .flex .profile');
-    let search = document.querySelector('.header .flex .search-form');
-    let userBtn = document.querySelector('#user-btn');
-    let menuBtn = document.querySelector('#menu-btn');
-    let closeBtn = document.querySelector('#close-btn');
-
-    userBtn.onclick = () => {
-        profile.classList.toggle('active');
-        search.classList.remove('active');
-    };
-
-    menuBtn.onclick = () => {
-        sideBar.classList.toggle('active');
-        body.classList.toggle('active');
-    };
-
-    closeBtn.onclick = () => {
-        sideBar.classList.remove('active');
-        body.classList.remove('active');
-    };
-
-    window.onscroll = () => {
-        profile.classList.remove('active');
-        search.classList.remove('active');
-        if (window.innerWidth < 1200) {
-            sideBar.classList.remove('active');
-            body.classList.remove('active');
-        }
-    };
-});*/
-
-
-document.addEventListener('DOMContentLoaded', function() {
-    let sideBar = document.querySelector('.side-bar');
-    let body = document.body;
 
     function updateSidebarVisibility() {
         if (window.innerWidth >= 1200) {
@@ -118,29 +76,49 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     };
 
-    // Get the current page URL
-    let currentPage = window.location.href;
+    let userBtn = document.getElementById('user-btn');
+    if (userBtn) {
+        userBtn.onclick = () => {
+            profile.classList.toggle('active');
+            search.classList.remove('active');
+        };
+    }
 
-    // Check if the current page is "sobreNos.html"
-    if (currentPage.includes("sobreNos.html")) {
-        // Remove the second div with id "user-btn"
-        let userBtn = document.getElementById('user-btn');
-        if (userBtn) {
-            userBtn.remove();
+    let notificationContainer = document.getElementById('notification-container');
+    if (notificationContainer) {
+        function showNotification(message, type) {
+            const notification = document.createElement('div');
+            notification.className = `notification ${type}`;
+            notification.textContent = message;
+
+            notificationContainer.innerHTML = '';
+            notificationContainer.appendChild(notification);
+
+            setTimeout(() => {
+                notificationContainer.innerHTML = '';
+            }, 5000);
         }
-    } else {
-        // Attach the event listener for userBtn if it exists
-        let userBtn = document.getElementById('user-btn');
-        if (userBtn) {
-            userBtn.onclick = () => {
-                profile.classList.toggle('active');
-                search.classList.remove('active');
-            };
-        }
+
+        const forms = document.querySelectorAll('form');
+
+        forms.forEach(form => {
+            form.addEventListener('submit', function (e) {
+                e.preventDefault();
+
+                try {
+                    const isSuccessful = true; // Altere para false para simular um erro
+
+                    if (isSuccessful) {
+                        showNotification('Ação realizada com sucesso!', 'success');
+                        // window.location.href = 'pagina_sucesso.html';
+                    } else {
+                        showNotification('Erro ao realizar a ação. Verifique suas informações e tente novamente.', 'error');
+                    }
+                } catch (error) {
+                    console.error('Erro ao processar formulário:', error);
+                }
+            });
+        });
     }
 });
 
-
-
-
- 
